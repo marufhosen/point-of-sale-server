92e77a3c995e35115a0bf01c3247bac83a999a98
Maruf Islam <maruf15-1591@diu.edu.bd>
2022-02-06 18:34:23 +0600

    service cres issue

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,7 +1,8 @@
 var admin = require("firebase-admin");
 require("dotenv").config();
 
-var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+var serviceAccount = require("../Config/pos-app-319-firebase-adminsdk-u7jjg-c8b3d3d493.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
